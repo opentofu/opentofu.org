c7121d60e54e1ba813e0196809759cac04a18451
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-09-05 17:49:48 +0200

    Update path to docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
         },
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          routeBasePath: "/",
+          routeBasePath: "/docs",
         },
       }),
     ],
