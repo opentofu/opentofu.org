4bddafc2a22dd6a3f0bbe809d5a457512c78299d
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-09-05 19:40:36 +0200

    Remove ticks around code

--- src/theme/DocItem/Content/index.js
+++ src/theme/DocItem/Content/index.js
@@ -3,7 +3,7 @@ import MDXContent from "@theme/MDXContent";
 
 export default function DocItemContent({ children }) {
   return (
-    <div className="prose lg:prose-lg prose-h1:text-4xl prose-h2:text-3xl dark:prose-invert">
+    <div className="prose lg:prose-lg prose-h1:text-4xl prose-h2:text-3xl dark:prose-invert prose-code:px-1.5 prose-code:before:content-none prose-code:after:content-none">
       <MDXContent>{children}</MDXContent>
     </div>
   );
