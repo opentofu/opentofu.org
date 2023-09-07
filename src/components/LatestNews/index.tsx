53ec7d9ddb913e9a4350bed9b8d8821970f4214d
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-09-07 15:51:15 +0200

    Remove console.log [skip ci]

--- src/components/LatestNews/index.tsx
+++ src/components/LatestNews/index.tsx
@@ -4,7 +4,6 @@ import Button from "../Button";
 import Link from "@docusaurus/Link";
 
 export default function LatestNews({ recentPosts }) {
-  console.log(recentPosts[0]);
   const {
     metadata: { title, formattedDate, frontMatter, permalink },
     Preview,
