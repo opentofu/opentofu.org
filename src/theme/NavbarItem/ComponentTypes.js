804e83fed09be420e977d7f69371d7c496d636d2
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-09-07 17:27:36 +0200

    Remove even more noise

--- src/theme/NavbarItem/ComponentTypes.js
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -1,23 +1,11 @@
+import ComponentTypes from "@theme-original/NavbarItem/ComponentTypes";
+
 import GitHubStarsNavbarItem from "@site/src/components/GitHubStarsNavbarItem";
 import ButtonNavbarItem from "@site/src/components/ButtonNavbarItem";
 import SocialIconLink from "@site/src/components/SocialIconLink";
 
-import DefaultNavbarItem from "@theme/NavbarItem/DefaultNavbarItem";
-import DropdownNavbarItem from "@theme/NavbarItem/DropdownNavbarItem";
-import SearchNavbarItem from "@theme/NavbarItem/SearchNavbarItem";
-import DocNavbarItem from "@theme/NavbarItem/DocNavbarItem";
-import DocSidebarNavbarItem from "@theme/NavbarItem/DocSidebarNavbarItem";
-import DocsVersionNavbarItem from "@theme/NavbarItem/DocsVersionNavbarItem";
-import DocsVersionDropdownNavbarItem from "@theme/NavbarItem/DocsVersionDropdownNavbarItem";
-
 export default {
-  default: DefaultNavbarItem,
-  search: SearchNavbarItem,
-  dropdown: DropdownNavbarItem,
-  doc: DocNavbarItem,
-  docSidebar: DocSidebarNavbarItem,
-  docsVersion: DocsVersionNavbarItem,
-  docsVersionDropdown: DocsVersionDropdownNavbarItem,
+  ...ComponentTypes,
   "custom-github-stars-navbar-item": GitHubStarsNavbarItem,
   "custom-button-navbar-item": ButtonNavbarItem,
   "custom-social-icon-link-navbar-item": SocialIconLink,
