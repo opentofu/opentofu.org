89576b5092b01a61a9a6a942608057d063fc1d76
Roman Grinovski <roman.grinovski@gmail.com>
2023-09-08 20:49:36 +0200

    tweak colors, fonts

---
+++ src/theme/PaginatorNavLink/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+export default function PaginatorNavLink(props) {
+  const { permalink, title, subLabel, isNext } = props;
+  return (
+    <Link
+      className={clsx(
+        "pagination-nav__link",
+        "border-gray-900 dark:border-gray-50",
+        isNext ? "pagination-nav__link--next" : "pagination-nav__link--prev"
+      )}
+      to={permalink}
+    >
+      {subLabel && <div className="pagination-nav__sublabel">{subLabel}</div>}
+      <div className="pagination-nav__label">{title}</div>
+    </Link>
+  );
+}
