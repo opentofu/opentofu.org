7d348e09bce2d000c6ae9e8bfe1632a23c1f2011
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-09-08 10:22:20 +0200

    Add image above the post's content

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useBlogPost } from "@docusaurus/theme-common/internal";
 import BlogPostItemContainer from "@theme/BlogPostItem/Container";
 import BlogPostItemHeader from "@theme/BlogPostItem/Header";
 import BlogPostItemContent from "@theme/BlogPostItem/Content";
@@ -7,9 +7,15 @@ import BlogPostItemContent from "@theme/BlogPostItem/Content";
 import type { Props } from "@theme/BlogPostItem";
 
 export default function BlogPostItem({ children }: Props) {
+  const { metadata } = useBlogPost();
+  const { title, frontMatter } = metadata;
   return (
     <BlogPostItemContainer>
       <BlogPostItemHeader />
+
+      <div className="container mx-auto flex justify-center">
+        <img src={frontMatter.image_url} alt={title} />
+      </div>
       <BlogPostItemContent>{children}</BlogPostItemContent>
     </BlogPostItemContainer>
   );
