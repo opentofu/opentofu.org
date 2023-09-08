3d573248a38bf87a6b2567240f5f06fa58bb4e3e
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-09-08 10:37:03 +0200

    Use TextContent component for blog post content

--- src/components/TextContent/index.tsx
+++ src/components/TextContent/index.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { HTMLProps } from "react";
 import clsx from "clsx";
 
-type TextContentProps = {
-  children: React.ReactNode;
-  className?: string;
-};
+type TextContentProps = HTMLProps<HTMLDivElement>;
 
-export default function TextContent({ children, className }: TextContentProps) {
+export default function TextContent({
+  children,
+  className,
+  ...rest
+}: TextContentProps) {
   return (
     <div
       className={clsx(
-        "prose lg:prose-lg mx-auto prose-h3:text-gray-900 dark:prose-h3:text-gray-50 prose-h3:text-2xl prose-h3:leading-snug md:prose-h3:text-5xl md:prose-h3:leading-normal prose-li:marker:text-inherit prose-a:text-inherit px-4 md:px-0 text-gray-600 dark:text-gray-500 mb-10",
+        "prose lg:prose-lg mx-auto prose-h3:text-gray-900 dark:prose-h3:text-gray-50 prose-h3:text-2xl prose-h3:leading-snug md:prose-h3:text-5xl md:prose-h3:leading-normal prose-li:marker:text-inherit prose-a:text-inherit px-4 md:px-0 text-gray-600 dark:text-gray-500 mb-10 prose-strong:text-inherit",
         className
       )}
+      {...rest}
     >
       {children}
     </div>
--- src/theme/BlogPostItem/Content/index.tsx
+++ src/theme/BlogPostItem/Content/index.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import { blogPostContainerID } from "@docusaurus/utils-common";
 import { useBlogPost } from "@docusaurus/theme-common/internal";
 import MDXContent from "@theme/MDXContent";
+import TextContent from "@site/src/components/TextContent";
 
 export default function BlogPostItemContent({ children }) {
   const { isBlogPostPage } = useBlogPost();
   return (
-    <div
-      className="prose lg:prose-xl dark:prose-invert mx-auto my-10"
+    <TextContent
+      className="my-4 md:my-10"
       id={isBlogPostPage ? blogPostContainerID : undefined}
       itemProp="articleBody"
     >
       <MDXContent>{children}</MDXContent>
-    </div>
+    </TextContent>
   );
 }
