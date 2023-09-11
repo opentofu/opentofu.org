c68a98fbc6fb82342c9dbb807e8bda5e1db85042
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-09-11 09:34:44 +0200

    Remove theme classes from paginator

---
+++ src/theme/DocPaginator/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PaginatorNavLink from "@theme/PaginatorNavLink";
+import type { Props } from "@theme/DocPaginator";
+
+export default function DocPaginator(props: Props) {
+  const { previous, next } = props;
+  return (
+    <nav className="grid grid-cols-2 gap-3 mt-6" aria-label="Docs pages">
+      {previous && <PaginatorNavLink {...previous} subLabel="Previous" />}
+      {next && <PaginatorNavLink {...next} subLabel="Next" isNext />}
+    </nav>
+  );
+}
--- src/theme/PaginatorNavLink/index.tsx
+++ src/theme/PaginatorNavLink/index.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
-export default function PaginatorNavLink(props) {
+import type { Props } from "@theme/PaginatorNavLink";
+
+export default function PaginatorNavLink(props: Props) {
   const { permalink, title, subLabel, isNext } = props;
   return (
     <Link
       className={clsx(
-        "pagination-nav__link",
-        "border-gray-900 dark:border-gray-50",
-        isNext ? "pagination-nav__link--next" : "pagination-nav__link--prev"
+        "border-gray-900 dark:border-gray-50 rounded-md border p-4",
+        isNext && "text-right"
       )}
       to={permalink}
     >
-      {subLabel && <div className="pagination-nav__sublabel">{subLabel}</div>}
-      <div className="pagination-nav__label">{title}</div>
+      {subLabel && (
+        <div className="text-sm text-gray-600 dark:text-gray-500">
+          {subLabel}
+        </div>
+      )}
+      <div className="font-bold">{title}</div>
     </Link>
   );
 }
