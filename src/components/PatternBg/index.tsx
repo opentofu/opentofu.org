41699dd80c82cf3c0ed17e24e7ffb4ab182a6b79
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-09-15 15:45:13 +0200

    Inline svg

--- src/components/PatternBg/index.tsx
+++ src/components/PatternBg/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./styles.module.css";
 
 export default function PatternBg() {
   return (
     <div className="absolute top-0 left-0 right-0 h-[600px] -z-10" aria-hidden>
       <div className="absolute inset-0 z-10 bg-gradient-to-b from-transparent to-gray-50 dark:to-dark1" />
-      <div className="bg-[url('/img/bg.svg')] w-full h-full opacity-5" />
+      <div className={`${styles.bg} w-full h-full opacity-5`} />
     </div>
   );
 }
