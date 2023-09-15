cca3caa3434a17ae02ee83d157cfed5a9caf7118
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-09-15 19:41:29 +0200

    Use colors from TW in as many places as possible

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class", '[data-theme="dark"]'],
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./docusaurus.config.js"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,svg}", "./docusaurus.config.js"],
   theme: {
     extend: {
       colors: {
