44d1634a569bccdc498d7cde9d6f567d81bfc222
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-09-17 13:11:29 +0200

    Slightly improve what's presented for SR in supporters section

--- src/components/Supporters/index.tsx
+++ src/components/Supporters/index.tsx
@@ -7,17 +7,23 @@ import { groupSupportersByType } from "../../utils/groupSupportersByType";
 import SupportersList from "../SupportersList";
 
 type SupporterTypeProps = {
-  children: React.ReactNode;
-  count?: number;
+  type: string;
+  count: number;
   withSeparator?: boolean;
 };
 
-function SupporterType({ children, withSeparator, count }: SupporterTypeProps) {
+function SupporterType({ type, withSeparator, count }: SupporterTypeProps) {
   return (
     <li className="h-12 flex items-center font-medium">
-      {children}
+      <span aria-hidden>{type}</span>
+      <span className="sr-only">
+        Supporting {type}: {count}
+      </span>
       {count && (
-        <sup className="ml-1 mt-2 text-yellow-700 dark:text-yellow-600 text-base font-bold">
+        <sup
+          className="ml-1 mt-2 text-yellow-700 dark:text-yellow-600 text-base font-bold"
+          aria-hidden
+        >
           {count}
         </sup>
       )}
@@ -42,17 +48,16 @@ export default function Supporters() {
       <h2 className="text-center text-3xl md:text-5xl font-bold mb-4 md:mb-7">
         Supporters
       </h2>
-      <ol className="inline-flex flex-wrap justify-center" role="list">
+      <ul className="inline-flex flex-wrap justify-center">
         {types.map(([type, supporters], index) => (
           <SupporterType
             key={type}
             count={supporters.length}
             withSeparator={index < types.length - 1}
-          >
-            {type}
-          </SupporterType>
+            type={type}
+          />
         ))}
-      </ol>
+      </ul>
       <SupportersList list={list} />
       <div className="flex gap-6 justify-center">
         <Button variant="secondary" href="/supporters">
