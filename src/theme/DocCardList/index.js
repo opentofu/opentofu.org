f7f9c517ad16d6769fd9c95a32a08b6d52da1456
Roman Grinovski <roman.grinovski@gmail.com>
2023-09-18 13:55:28 +0200

    add docs index page (#94)
    * add docs index page
    
    * improve DocCardList styles
    
    * adjust styles of docs index

---
+++ src/theme/DocCardList/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import clsx from "clsx";
+import {
+  useCurrentSidebarCategory,
+  filterDocCardListItems,
+} from "@docusaurus/theme-common";
+import DocCard from "@theme/DocCard";
+function DocCardListForCurrentSidebarCategory({ className }) {
+  const category = useCurrentSidebarCategory();
+  return <DocCardList items={category.items} className={className} />;
+}
+export default function DocCardList(props) {
+  const { items, className } = props;
+  if (!items) {
+    return <DocCardListForCurrentSidebarCategory {...props} />;
+  }
+  const filteredItems = filterDocCardListItems(items);
+  return (
+    <section
+      className={clsx(
+        "grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 w-full",
+        className
+      )}
+    >
+      {filteredItems.map((item, index) => (
+        <article key={index} className="not-prose">
+          <DocCard item={item} />
+        </article>
+      ))}
+    </section>
+  );
+}
