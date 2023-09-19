6339dd343788d7c1ded6c879f49e08a49a13b4c8
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-09-19 12:45:35 +0200

    Simplify page titles

--- src/pages/manifesto.tsx
+++ src/pages/manifesto.tsx
@@ -8,7 +8,7 @@ import Link from "@docusaurus/Link";
 export default function Manifesto() {
   return (
     <Layout
-      title="The OpenTofu Manifesto"
+      title="Manifesto"
       description="Terraform was open-sourced in 2014 under the Mozilla Public License
     (v2.0) (the “MPL”). Over the next ~9 years, it built up a community
     that included thousands of users, contributors, customers, certified
