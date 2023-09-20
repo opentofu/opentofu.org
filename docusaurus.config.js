9e983f7cf3c43c11eed44154786c83ae3c976ff3
Roman Grinovski <roman.grinovski@gmail.com>
2023-09-20 11:48:22 +0200

    add editUrl to docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,6 +84,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           routeBasePath: "/docs",
+          editUrl: "https://github.com/opentofu/opentofu/tree/main/website",
         },
         blog: false,
         gtag: {
