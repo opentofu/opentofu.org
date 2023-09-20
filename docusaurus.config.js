bf67d7901435bdc1e85be0c3656b03a3b535c785
Roman Grinovski <roman.grinovski@gmail.com>
2023-09-20 11:51:54 +0200

    change link to actual editing link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           routeBasePath: "/docs",
-          editUrl: "https://github.com/opentofu/opentofu/tree/main/website",
+          editUrl: "https://github.com/opentofu/opentofu/edit/main/website",
         },
         blog: false,
         gtag: {
