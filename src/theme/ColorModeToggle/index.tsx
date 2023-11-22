d357855fb97ddb4cb5a1fcd9818ba7b215f8b134
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-11-22 12:51:20 +0100

    Swizzle color mode component
    Signed-off-by: Damian Stasik <920747+damianstasik@users.noreply.github.com>

---
+++ src/theme/ColorModeToggle/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import clsx from "clsx";
+import useIsBrowser from "@docusaurus/useIsBrowser";
+
+import IconLightMode from "@theme/Icon/LightMode";
+import IconDarkMode from "@theme/Icon/DarkMode";
+import type { Props } from "@theme/ColorModeToggle";
+
+function ColorModeToggle({ value, onChange }: Props) {
+  const isBrowser = useIsBrowser();
+
+  const title = `Switch between dark and light mode (currently ${
+    value === "dark" ? "dark mode" : "light mode"
+  })`;
+
+  return (
+    <button
+      className="transition-colors text-gray-900 hover:text-brand-700 dark:text-gray-50 dark:hover:text-brand-500"
+      type="button"
+      onClick={() => onChange(value === "dark" ? "light" : "dark")}
+      disabled={!isBrowser}
+      title={title}
+      aria-label={title}
+      aria-live="polite"
+    >
+      {value === "light" && <IconLightMode />}
+      {value === "dark" && <IconDarkMode />}
+    </button>
+  );
+}
+
+export default React.memo(ColorModeToggle);
