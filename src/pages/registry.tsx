5c4156b320685b24bc57c58dec40412e0fb12b3a
Christian Mesh <christianmesh1@gmail.com>
2023-12-08 09:12:33 -0500

    Add simple registry landing page for OpenTofu/OpenTofu#942 (#232)
    Signed-off-by: Christian Mesh <christianmesh1@gmail.com>

---
+++ src/pages/registry.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "@theme/Layout";
+import Jumbotron from "../components/Jumbotron";
+import Link from "@docusaurus/Link";
+import Headline from "../components/Headline";
+import TextContent from "../components/TextContent";
+
+export default function FAQ() {
+  return (
+    <Layout title="Registry">
+      <Jumbotron>
+        <Headline>OpenTofu Registry</Headline>
+      </Jumbotron>
+      <TextContent className="mb-4 md:mb-10 mx-auto px-4">
+        The OpenTofu Registry contains providers and modules to be used with OpenTofu. In order to view the catalogue of providers / modules, or
+        if you’d like to submit new providers / modules, please head to <Link href="https://github.com/opentofu/registry/">https://github.com/opentofu/registry/</Link>.
+      </TextContent>
+    </Layout>
+  );
+}
