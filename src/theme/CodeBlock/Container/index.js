9087a402abf0e600a6b0582ff3814f03bba0218f
Shashwat Pandey <72958686+SypherSP@users.noreply.github.com>
2024-03-07 17:51:30 +0530

    issue 260: updating codeblocks for better screen reader compatibility  (#274)
    * added implementation of CodeBlock that wraps it in a figure
    
    Signed-off-by: Shashwat Pandey <72958686+SypherSP@users.noreply.github.com>
    
    * added comment for swizzled files
    
    Signed-off-by: Shashwat Pandey <72958686+SypherSP@users.noreply.github.com>
    
    * updated location of role tag
    
    Signed-off-by: Shashwat Pandey <72958686+SypherSP@users.noreply.github.com>
    
    * fixed typo
    
    Signed-off-by: Shashwat Pandey <72958686+SypherSP@users.noreply.github.com>
    
    * copy button rearranged to come before codeblock
    
    Signed-off-by: Shashwat Pandey <72958686+SypherSP@users.noreply.github.com>
    
    * added <figure> wrapper back
    
    Signed-off-by: Shashwat Pandey <72958686+SypherSP@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Shashwat Pandey <72958686+SypherSP@users.noreply.github.com>

---
+++ src/theme/CodeBlock/Container/index.js
@@ -0,0 +1,23 @@
+// This file has been swizzled from Docusaurus
+
+import React from "react";
+import clsx from "clsx";
+import { ThemeClassNames, usePrismTheme } from "@docusaurus/theme-common";
+import { getPrismCssVariables } from "@docusaurus/theme-common/internal";
+import styles from "./styles.module.css";
+export default function CodeBlockContainer({ as: As, ...props }) {
+  const prismTheme = usePrismTheme();
+  const prismCssVariables = getPrismCssVariables(prismTheme);
+  return (
+    <As
+      // Polymorphic components are hard to type, without `oneOf` generics
+      {...props}
+      style={prismCssVariables}
+      className={clsx(
+        props.className,
+        styles.codeBlockContainer,
+        ThemeClassNames.common.codeBlock,
+      )}
+    />
+  );
+}
